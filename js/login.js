6062c490a6dc5f75cb1008f547a241c0fe995b2a
Lynn <pely24wt@student.ju.se>
2025-05-15 20:09:58 +0200

    login done

---
+++ js/login.js
@@ -0,0 +1,34 @@
+const loginButton = document.getElementById("login-button");
+
+if (loginButton) {
+  loginBtn.addEventListener("click", () => {
+    const username = document.getElementById("loginUsername").value.trim();
+    const password = document.getElementById("loginPassword").value;
+
+    if (!username || !password) {
+      alert("Please enter both username and password.");
+      return;
+    }
+
+    let usersData = localStorage.getItem("users");
+    let users;
+
+    if (usersData) {
+      users = JSON.parse(usersData);
+    } else {
+      users = [];
+    }
+
+    const matchingUser = users.find(
+      (user) => user.username === username && user.password === password
+    );
+
+    if (matchingUser) {
+      alert("Login succesfull");
+      localStorage.setItem("loggedInUser", JSON.stringify(matchingUser));
+      window.location.href = "index.html";
+    } else {
+      alert("Incorrect username or password.");
+    }
+  });
+}
